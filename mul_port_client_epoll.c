7053d35d6a52954419b609b63069c7f013726fe2
cumtbhl <2213538105@qq.com>
2025-01-06 21:19:33 +0800

    看懂mul_port_client_epoll.c的代码，实现2.2服务器百万并发

--- mul_port_client_epoll.c
+++ mul_port_client_epoll.c
@@ -1,3 +1,6 @@
+
+// 本代码进行服务器端高并发测试，模拟大量客户端并发连接到服务器
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -14,11 +17,12 @@
 #define MAX_BUFFER		128
 #define MAX_EPOLLSIZE	(384*1024)
 #define MAX_PORT		20
-
 #define TIME_SUB_MS(tv1, tv2)  ((tv1.tv_sec - tv2.tv_sec) * 1000 + (tv1.tv_usec - tv2.tv_usec) / 1000)
 
 int isContinue = 0;
 
+//套接字 fd 设置为非阻塞模式，套接字的读写操作不会被阻塞
+//如果没有数据可读或不能写数据时，操作会立即返回
 static int ntySetNonblock(int fd) {
 	int flags;
 
@@ -29,14 +33,17 @@ static int ntySetNonblock(int fd) {
 	return 0;
 }
 
+//设置 SO_REUSEADDR，操作系统允许在 TIME_WAIT 状态期间新的客户端重新绑定该端口
 static int ntySetReUseAddr(int fd) {
 	int reuse = 1;
 	return setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse));
 }
 
 
+//argv: 指 向字符串指针类型 的 指针
+int main(int argc, char** argv) {
 
-int main(int argc, char **argv) {
+	//传入服务器端的 ip 和 port
 	if (argc <= 2) {
 		printf("Usage: %s ip port\n", argv[0]);
 		exit(0);
@@ -50,6 +57,7 @@ int main(int argc, char **argv) {
 
 	struct epoll_event events[MAX_EPOLLSIZE];
 	
+	//为 epoll_fd 指定一个较大的缓冲区
 	int epoll_fd = epoll_create(MAX_EPOLLSIZE);
 	
 	strcpy(buffer, " Data From MulClient\n");
@@ -76,7 +84,7 @@ int main(int argc, char **argv) {
 				goto err;
 			}
 
-			//ntySetReUseAddr(sockfd);
+			//sockfd 复用服务器端的 0-19端口号
 			addr.sin_port = htons(port+index);
 
 			if (connect(sockfd, (struct sockaddr*)&addr, sizeof(struct sockaddr_in)) < 0) {
@@ -86,6 +94,8 @@ int main(int argc, char **argv) {
 			ntySetNonblock(sockfd);
 			ntySetReUseAddr(sockfd);
 
+			//对于每次新生成的一个客户端 sockfd
+			//服务器端都会发送 "Hello Server: client --> %d\n" 给该客户端
 			sprintf(buffer, "Hello Server: client --> %d\n", connections);
 			send(sockfd, buffer, strlen(buffer), 0);
 
@@ -95,44 +105,64 @@ int main(int argc, char **argv) {
 		
 			connections ++;
 		}
-		//connections ++;
+
+		//每连接了个 1000 个客户端时，或者客户端连接数到达上限时
 		if (connections % 1000 == 999 || connections >= 340000) {
 			struct timeval tv_cur;
 			memcpy(&tv_cur, &tv_begin, sizeof(struct timeval));
 			
 			gettimeofday(&tv_begin, NULL);
 
+			//检查每连接 1000 个客户端所花费的时间
 			int time_used = TIME_SUB_MS(tv_begin, tv_cur);
 			printf("connections: %d, sockfd:%d, time_used:%d\n", connections, sockfd, time_used);
 
+			//处理当前已连接的 connections 个客户端的事件
 			int nfds = epoll_wait(epoll_fd, events, connections, 100);
 			for (i = 0;i < nfds;i ++) {
 				int clientfd = events[i].data.fd;
 
+				//如果客户端 clientfd 可写，则发送 "data from %d\n"
 				if (events[i].events & EPOLLOUT) {
 					sprintf(buffer, "data from %d\n", clientfd);
 					send(sockfd, buffer, strlen(buffer), 0);
-				} else if (events[i].events & EPOLLIN) {
+				} 
+
+				//如果客户端 clienfd 可读
+				else if (events[i].events & EPOLLIN) {
 					char rBuffer[MAX_BUFFER] = {0};				
 					ssize_t length = recv(sockfd, rBuffer, MAX_BUFFER, 0);
+
+					//如果客户端发了数据，则打印从 clientfd 接收到的数据
 					if (length > 0) {
 						printf(" RecvBuffer:%s\n", rBuffer);
 
+						//strcmp(str1, str2): 如果str1 = str2 ，则返回0
+						//当某个客户端发来 "quit" 时，则设置isContinue = 0
 						if (!strcmp(rBuffer, "quit")) {
 							isContinue = 0;
 						}
 						
-					} else if (length == 0) {
+					} 
+					//如果客户端主动断开连接，则打印 " Disconnect clientfd:%d\n"
+					else if (length == 0) {
 						printf(" Disconnect clientfd:%d\n", clientfd);
 						connections --;
 						close(clientfd);
-					} else {
+					} 
+
+					//如果 recv() 返回负值时，表示在接收数据时发生了错误
+					else {
 						if (errno == EINTR) continue;
 
 						printf(" Error clientfd:%d, errno:%d\n", clientfd, errno);
 						close(clientfd);
 					}
-				} else {
+				} 
+
+				//如果客户端的事件类型既不包含 EPOLLOUT 也不包含 EPOLLIN
+				//则表示这是一个未知的事件类型，或者发生了错误
+				else {
 					printf(" clientfd:%d, errno:%d\n", clientfd, errno);
 					close(clientfd);
 				}
@@ -147,8 +177,4 @@ int main(int argc, char **argv) {
 err:
 	printf("error : %s\n", strerror(errno));
 	return 0;
-	
 }
-
-
-
